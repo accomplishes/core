7bb37690677d17cc2f5d1cb14328c49f982f32d3
Jamie Holdroyd <jamie@moltin.com>
2017-09-20 03:00:34 +0100

    Add multi directory support to locales

--- app/libs/locale/index.js
+++ app/libs/locale/index.js
@@ -3,9 +3,11 @@
 // Load our requirements
 const i18n = require('i18n')
 const path = require('path')
+const fs = require('fs')
 
 // Variables
-let localesDir = path.resolve(path.join(__dirname, '../../../locales'))
+const localesDir = path.join(__dirname, '../../../locales')
+const customDir = path.join(localesDir, '../data/achievements/custom/locales')
 
 // Configure the localization engine
 i18n.configure({
@@ -13,14 +15,34 @@ i18n.configure({
   defaultLocale: 'en',
   objectNotation: true,
   directory: localesDir,
+  multiDirectories: true,
   autoReload: true,
-  syncFiles: true,
   register: i18n,
   api: {
     '__': 't',
     '__n': 'tn'
   }
 })
+
+// Add alias register helper
+i18n.register = function (dir) {
+  // Ensure directory is absolute
+  /* istanbul ignore next */
+  if (!path.isAbsolute(dir)) {
+    dir = path.resolve(dir)
+  }
+
+  // Ensure directory exists
+  if (!fs.existsSync(dir) || !fs.lstatSync(dir).isDirectory()) {
+    return
+  }
+
+  // Register and return
+  return i18n.configure({directory: dir})
+}
+
+// Auto register custom achievement locles
+i18n.register(customDir)
 
 // Export for future use
 module.exports = i18n
